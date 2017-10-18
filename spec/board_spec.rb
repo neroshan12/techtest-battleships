bf503e5b20905aacccc241bd9acac9dd94d31f7a
Nero Siva <neroshan12@gmail.com>
2017-10-18 10:56:20 +0100

    Rename files for better flow

---
+++ spec/board_spec.rb
@@ -0,0 +1,15 @@
+require 'board'
+
+describe Board do
+  subject(:board) {described_class.new}
+
+  context '#initialize' do
+    it "allows a board to be initialized" do
+      expect(board).to be_instance_of(Board)
+    end
+
+    it "initializes with a 10x10 grid" do
+      expect(board.grid).to eq([[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]])
+    end
+  end
+end
