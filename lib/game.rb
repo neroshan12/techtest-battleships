eaac5fd1efca879f5c29f3de1d9801e4f7478bfc
Nero Siva <neroshan12@gmail.com>
2017-10-19 11:10:22 +0100

    Fix failing tests

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,16 @@
+require './lib/board'
+
 class Game
-  def initialize
-    
+  # def initialize(board_class = Board)  # here if nothing is passed in the Board will be passed in and L6 board_class will be board
+  #   @board_class = board_class # if you pass in a board_class it acts as if it would do
+  #   @board = board_class.new
+  # end
+
+  def initialize(board = Board.new)
+    @board = board
+  end
+
+  def gamegrid(board)
+    @board.grid
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,11 +1,33 @@
 require 'game'
+require 'board'
 
 describe Game do
-  subject(:game) {described_class.new}
+  # subject(:game) {described_class.new(board_class)}     # game is instantiated with a fake board_class object that acts like a board
+  # let(:board_class) {double :board_class, new: board }  # here the double is saying that this fake class board_class which will act like a board will respond to the new method
+  # let(:board) {double :board}                           # and that board will act like a board
+board = Board.new
+subject(:game) {described_class.new(board)}
 
   context '#initialize' do
     it 'initializes a new game' do
+    #  board = Board.new
+      # game = Game.new(board)
       expect(game).to be_instance_of(Game)
+    end
+    it 'initializes with a new empty grid' do
+      # board = Board.new
+      #puts game.gamegrid(board_class)
+      expect(game.gamegrid(board)).to eq([[0,0,0,0,0,0,0,0,0,0],
+                                                 [0,0,0,0,0,0,0,0,0,0],
+                                                 [0,0,0,0,0,0,0,0,0,0],
+                                                 [0,0,0,0,0,0,0,0,0,0],
+                                                 [0,0,0,0,0,0,0,0,0,0],
+                                                 [0,0,0,0,0,0,0,0,0,0],
+                                                 [0,0,0,0,0,0,0,0,0,0],
+                                                 [0,0,0,0,0,0,0,0,0,0],
+                                                 [0,0,0,0,0,0,0,0,0,0],
+                                                 [0,0,0,0,0,0,0,0,0,0]
+                                                 ])
     end
   end
 end
