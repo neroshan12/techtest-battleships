4aecf5a371aa51c49b3e62a3d8b43fc0c187ea95
Nero Siva <neroshan12@gmail.com>
2017-10-20 08:35:52 +0100

    Refactor ship test

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,10 +1,13 @@
 require 'ship'
 
 describe Ship do
+
+  subject(:ship) {described_class.new}
+
   context '#initializes with a ship size' do
     it 'creates a ship of size 3' do
-      ship = Ship.new
       expect(ship.size).to eq(3)
     end
   end
+  
 end
